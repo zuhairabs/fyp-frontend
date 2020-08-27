18e8d5e85e747b05ef97769afe1d36dc06f7d4d8
Suryansh Sugandhi <suryansh71199@gmail.com>
2020-08-27 23:05:24 +0530

    Fix: Notification exit state behaviour

--- controllers/Notifications/NotificationListener.js
+++ controllers/Notifications/NotificationListener.js
@@ -1,24 +1,33 @@
 import messaging from '@react-native-firebase/messaging';
 import {navigationRef} from '../../Navigation/Navigation';
 import {fetchNotifications} from './NotificationHandler';
+import {ToastAndroid} from 'react-native';
+
+const redirectPushNotification = (remoteMessage) => {
+  if (remoteMessage.data?.booking)
+    navigationRef.current?.navigate('SingleBooking', {
+      booking: remoteMessage.data.booking,
+      archived: remoteMessage.data.archived,
+    });
+  else if (remoteMessage.data?.store)
+    navigationRef.current?.navigate('Store', {
+      store: remoteMessage.data.store,
+      searched: false,
+    });
+};
 
 export default NotificationListener = (setNotifications) => {
   // Handler to control push notification interaction
   messaging().onNotificationOpenedApp((remoteMessage) => {
-    if (remoteMessage.data?.booking)
-      navigationRef.current?.navigate('SingleBooking', {
-        booking: remoteMessage.data.booking,
-        archived: remoteMessage.data.archived,
-      });
-    else if (remoteMessage.data?.store)
-      navigationRef.current?.navigate('Store', {
-        store: remoteMessage.data.store,
-        searched: false,
-      });
+    redirectPushNotification(remoteMessage);
+  });
+
+  messaging().getInitialNotification((remoteMessage) => {
+    redirectPushNotification(remoteMessage);
   });
 
   // Global message handler
-  messaging().onMessage(async (_) => {
+  messaging().onMessage(async (remoteMessage) => {
     fetchNotifications(setNotifications);
   });
 };
--- index.js
+++ index.js
@@ -2,14 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, ToastAndroid} from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 import App from './App';
 import {name as appName} from './app.json';
 import {fetchNotifications} from './controllers/Notifications/NotificationHandler';
 
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-  console.log('A new message has arrived for ShopOut', remoteMessage);
   fetchNotifications();
 });
 
