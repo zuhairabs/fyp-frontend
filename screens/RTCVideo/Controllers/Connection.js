3edf630f949efa63ec2e2e45b7dd1bc22e2f30cc
Suryansh Sugandhi <suryansh71199@gmail.com>
2020-09-13 21:30:09 +0530

    add: rtc video routes

--- screens/RTCVideo/Controllers/Connection.js
+++ screens/RTCVideo/Controllers/Connection.js
@@ -1,5 +1,5 @@
-import {Post} from '../../../api/http';
-const registerRoute = 'user/video-rtc/newParticipant';
+import {PostBaseRoute} from '../../../api/http';
+const registerRoute = 'rtc-video/register';
 
 export const appId = 'de22f355862e48539cb856e69aa4d557';
 export const generateRandomUid = () => {
@@ -14,7 +14,7 @@ export const registerNewParticipant = (ref_id, channelName, uid) =>
       channelName,
       uid,
     });
-    Post(registerRoute, body)
+    PostBaseRoute(registerRoute, body)
       .then(() => resolve())
       .catch((e) => reject(e));
   });
