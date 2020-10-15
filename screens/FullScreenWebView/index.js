4543097b35e98f9791e6916f406990d804a82a2b
Suryansh Sugandhi <suryansh71199@gmail.com>
2020-10-15 14:32:58 +0530

    add: tracking to webview pages

--- screens/FullScreenWebView/index.js
+++ screens/FullScreenWebView/index.js
@@ -1,15 +1,52 @@
-import React, {useRef, useState} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import {Dimensions, StyleSheet, View} from 'react-native';
 import {WebView} from 'react-native-webview';
 import ProgressBar from '../../components/ProgressBar';
-
+import {Post} from '../../api/http';
+import {GlobalContext} from '../../providers/GlobalContext';
 import {COLORS} from '../../styles/styles';
+
 const {height} = Dimensions.get('window');
+const sessionURL = 'app/tracker/history/session';
 
 export default ({route}) => {
   const {uri} = route.params;
-  const [loadProgress, setLoadProgress] = useState(0);
+  const {state} = useContext(GlobalContext);
   const webViewRef = useRef();
+
+  const [sessionId, setSessionId] = useState();
+  const [loadProgress, setLoadProgress] = useState(0);
+
+  const generateSession = async () => {
+    try {
+      const body = JSON.stringify({
+        cred: {
+          phone: state.user.phone,
+        },
+      });
+      const response = await Post(`${sessionURL}/start`, body);
+      setSessionId(response.sessionId);
+      addToSession(uri, response.sessionId);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const addToSession = async (url, sessionId) => {
+    try {
+      const body = JSON.stringify({
+        sessionId,
+        url,
+      });
+      await Post(`${sessionURL}/add`, body);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    generateSession();
+  }, []);
 
   return (
     <View style={styles.screenContainer}>
@@ -24,7 +61,10 @@ export default ({route}) => {
         javaScriptEnabled
         domStorageEnabled
         onLoad={({nativeEvent}) => {
-          if (!nativeEvent.loading) console.log({nativeEvent});
+          if (!nativeEvent.loading) {
+            console.log({nativeEvent});
+            addToSession(nativeEvent.url, sessionId);
+          }
         }}
         onLoadProgress={({nativeEvent}) =>
           setLoadProgress(nativeEvent.progress)
